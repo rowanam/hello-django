9d3d1fefdd78b9c4dcdde193112b73db83800e1d
rowanam <rowanamaas@gmail.com>
2023-11-21 15:34:59 +0000

    Link external database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-if os.path.exists("env.py"):
+import dj_database_url
+if os.path.isfile("env.py"):
     import env  # pylint: disable=unused-import
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["8000-rowanam-hellodjango-vdkevq0vnk7.ws-eu106.gitpod.io"]
+ALLOWED_HOSTS = ["8000-rowanam-hellodjango-vdkevq0vnk7.ws-eu106.gitpod.io", "localhost"]
 
 
 # Application definition
@@ -75,11 +76,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
